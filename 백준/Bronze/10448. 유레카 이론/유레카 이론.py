ada65c7295c8065a809357b540f1c0e56a4a44e3
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-03-15 22:28:38 +0900

    [Bronze I] Title: 유레카 이론, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.py
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.py
@@ -2,29 +2,25 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-data = list(int(input()) for _ in range(N))
-maximum = 0
-answer = list(range(N))
+data = [int(input()) for _ in range(N)]
+answer = [0] * N
 
 def solve(target):
+  maximum = 0
   for i in range(1, 46):
-    if i * (i + 1) / 2 >= target :
+    if i * (i + 1) // 2 >= target:
       maximum = i
-      break  
+      break
+  
   for i in range(1, maximum):
     for j in range(1, maximum):
-      for k in range (1, maximum):
-        tmp = ( i * (i + 1) / 2 ) + ( j * (j + 1) / 2) + ( k * (k + 1) / 2)
+      for k in range(1, maximum):
+        tmp = (i * (i + 1) // 2) + (j * (j + 1) // 2) + (k * (k + 1) // 2)
         if target == tmp:
-          return 1;
-  else:
-    return 0;
+          return 1
+  return 0
 
 for i in range(N):
   answer[i] = solve(data[i])
 
-for num in answer:
-  print(num)
-  
-
-
+print("\n".join(map(str, answer)))
