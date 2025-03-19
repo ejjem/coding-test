8acc6ed38fcbe6345776761e426ab946e4e2b1ee
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-03-20 06:30:26 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -6,23 +6,24 @@ board = [list(input().rstrip()) for _ in range(N)]
 
 answer = float('inf')
 
-for row_start in range(N - 7):  # 8x8 크기의 체스판을 탐색
-    for col_start in range(M - 7):
-        cnt1, cnt2 = 0, 0  # 첫 칸을 'W'로 시작하는 경우(cnt1), 'B'로 시작하는 경우(cnt2)
-
-        for i in range(8):
-            for j in range(8):
-                if (i + j) % 2 == 0:  # 짝수 위치
-                    if board[row_start + i][col_start + j] != 'W':
-                        cnt1 += 1
-                    if board[row_start + i][col_start + j] != 'B':
-                        cnt2 += 1
-                else:  # 홀수 위치
-                    if board[row_start + i][col_start + j] != 'B':
-                        cnt1 += 1
-                    if board[row_start + i][col_start + j] != 'W':
-                        cnt2 += 1
-        
-        answer = min(answer, cnt1, cnt2)
+for row in range(N - 7):
+  for col in range(M - 7):
+    cnt_1, cnt_2 = 0, 0
+    
+    for i in range(8):
+      for j in range(8):
+        if (i + j) % 2 == 0:
+          if board[row + i][col + j] != 'W':
+            cnt_1 += 1
+          if board[row + i][col + j] != 'B':
+            cnt_2 += 1
+        else:
+          if board[row + i][col + j] != 'B':
+            cnt_1 += 1
+          if board[row + i][col + j] != 'W':
+            cnt_2 += 1
+    
+    answer = min(answer, cnt_1, cnt_2)
 
 print(answer)
+      
