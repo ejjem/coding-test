5fee23d4b6d285789e459d6fa0d59a0150416d4f
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-03-26 01:41:00 +0900

    [Silver I] Title: 스타트와 링크, Time: 2416 ms, Memory: 58996 KB -BaekjoonHub

---
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -0,0 +1,35 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+N = int(input())
+member = [x for x in range(1, N+1)]
+point = [[x for x in map(int, input().split())] for _ in range(N)]
+minimum = float("inf")
+comb = [arr for arr in combinations(member, N//2)]  
+  
+team_start= comb[:len(comb)//2]
+team_link = comb[len(comb)//2: ]
+team_link.reverse()
+
+for i in range(len(team_start)):
+  team_start_point = 0
+  team_link_point = 0
+  team_start_target = team_start[i]
+  team_link_target = team_link[i]
+  for x, y in combinations(team_start_target, 2):
+    team_start_point += point[x-1][y-1] + point[y-1][x-1]
+  for x, y in combinations(team_link_target, 2):
+    team_link_point += point[x-1][y-1] + point[y-1][x-1]
+  
+  minimum = min(minimum, abs(team_start_point - team_link_point))
+
+print(minimum)
+  
+    
+
+  
+
+
+
+
