7571eb605d956a07aac4925ab861e00b2614562a
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-03-29 04:15:40 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -5,52 +5,37 @@ T = int(input())
 answer = []
 
 def search(bat, M, N):
-  global answer
-  checked = set()
-  count = 0
-  for X in range(M):
-    for Y in range(N):
-      if bat[Y][X] == 1 and (X, Y) not in checked:
-        tmp = set()
-        DFS(X, Y, checked)
-        count += 1
-  answer.append(count)      
-          
-def DFS(X, Y, checked):
-  global M, N, bat
-  stack = [(X, Y)]
-  while stack:
-    target_X, target_Y = stack.pop()
-    if (target_X, target_Y) not in checked:
-      checked.add((target_X, target_Y))
-      tmp = set()
-      if 0 <= target_X+1  and target_X+1 <= (M-1):
-        if bat[target_Y][target_X+1] == 1 and (target_X+1, target_Y) not in checked:
-          tmp.add((target_X+1, target_Y))
-      if 0 <= target_X-1  and target_X-1 <= (M-1):
-        if bat[target_Y][target_X-1] == 1 and (target_X-1, target_Y) not in checked:
-          tmp.add((target_X-1, target_Y))
-      if 0 <= target_Y+1  and target_Y+1 <= (N-1):
-        if bat[target_Y+1][target_X] == 1 and (target_X, target_Y+1) not in checked:
-          tmp.add((target_X, target_Y+1))
-      if 0 <= target_Y-1  and target_Y-1 <= (N-1):
-        if bat[target_Y-1][target_X] == 1 and (target_X, target_Y-1) not in checked:
-          tmp.add((target_X, target_Y-1))
-      for node in tmp:
-        stack.append(node)
-        
-  
+    checked = set()
+    count = 0
+    for X in range(M):
+        for Y in range(N):
+            if bat[Y][X] == 1 and (X, Y) not in checked:
+                DFS(X, Y, checked, bat, M, N)
+                count += 1
+    answer.append(count)      
 
-for i in range(T):
-  M, N, K = map(int, input().split())
-  bat = []
-  bat = [[0]*M for _ in range(N)]  
-  for _ in range(K):
-    X, Y = map(int, input().split())
-    bat[Y][X] = 1
-  search(bat, M, N)
-  
+def DFS(X, Y, checked, bat, M, N):
+    stack = [(X, Y)]
+    checked.add((X, Y))
+
+    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    while stack:
+        x, y = stack.pop()
+        for dx, dy in directions:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < M and 0 <= ny < N:
+                if bat[ny][nx] == 1 and (nx, ny) not in checked:
+                    checked.add((nx, ny))
+                    stack.append((nx, ny))
+
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    bat = [[0] * M for _ in range(N)]  
+    for _ in range(K):
+        X, Y = map(int, input().split())
+        bat[Y][X] = 1
+    search(bat, M, N)
+
 for i in answer:
-  print(i)
-    
-  
+    print(i)
