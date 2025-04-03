3d503e87ff56196315eaf9f10cb2acd097731778
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-04-03 15:57:51 +0900

    [Silver III] Title: 숫자 정사각형, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
+++ 백준/Silver/1051. 숫자 정사각형/숫자 정사각형.py
@@ -2,25 +2,31 @@ import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-data = [list(map(int, input().rstrip())) for _ in range(N)]
+data = []
+for _ in range(N):
+  data.append(list(map(int, input().rstrip())))
 
-max_length = min(N, M)
-max_gap = max_length - 1
 answer = 1
-for gap in range(1 , max_gap + 1): # gap: 0 ~ max_gap
-    #print(f"gap: {gap}")
-    row, col = 0, 0
-    
-    for row in range(N - gap): # row: 0 ~ N - gap - 1
-        for col in range(M - gap): # col: 0 ~ M - gap - 1
-         tmp = data[row][col] #왼쪽 상단 꼭짓점
-         #print(f"tmp: {tmp}")
-         #print(f"gap: {gap}")
-         #print(f"col: {col}")
-         #print(f"row: {row}")
-         
-         if data[row][col + gap] == tmp and data[row + gap][col] == tmp and data[row + gap][col + gap] == tmp:
-             answer = max(answer, gap + 1)
-             #print(f"answer: {answer}")
-                         
+max_length = min(N, M) - 1
+def search(max_length, data):
+  for length in range(max_length, 0, -1):
+    for row in range(N):
+      if row + length >= N:
+        break
+      for col in range(M):
+        if col + length >= M:
+          break
+        standard = data[row][col]
+        targets = [data[row+length][col], data[row][col+length], data[row+length][col+length]]
+        if all(target == standard for target in targets):
+            return length     
+  return 0
+      
+answer = search(max_length, data) + 1
 print(answer ** 2)
+
+
+
+    
+  
+  
