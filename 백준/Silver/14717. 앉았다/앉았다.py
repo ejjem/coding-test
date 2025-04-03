f1314bb09d57c30a8b2551614cf231ee69cc8731
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-04-03 14:10:56 +0900

    [Silver IV] Title: 앉았다, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14717. 앉았다/앉았다.py
+++ 백준/Silver/14717. 앉았다/앉았다.py
@@ -1,33 +1,20 @@
-from itertools import combinations
 import sys
+from itertools import combinations
 input = sys.stdin.readline
 
-cards = [i for i in range(1, 11)] * 2
-
-lose = 0
-total = 18 * 17 / 2  # 상대방이 가질 수 있는 모든 경우의 수
-
 A, B = map(int, input().split())
-
-cards.remove(A)
-cards.remove(B)
-
-# 내 족보 계산
+total = 18 * 17 / 2
+win = 0
 if A == B:
-    my_rank = (10 + A)  # 땡
+  lose = 10 - A
+  win = total - lose
 else:
-    my_rank = (A + B) % 10  # 끗
-
-# 상대방 가능한 패 모든 조합 구하기 (O(N^2))
-for opp_A, opp_B in combinations(cards, 2):
-    if opp_A == opp_B:
-        opp_rank = (10 + opp_A)  # 상대가 땡
-    else:
-        opp_rank = (opp_A + opp_B) % 10  # 상대가 끗
-    
-    # 승패 비교
-    if my_rank <= opp_rank:
-        lose += 1  # 내가 진 경우만 카운트
-        
-answer = (total - lose) / total
+  card = list(range(1, 11)) * 2
+  card.remove(A)
+  card.remove(B)
+  score = (A + B) % 10
+  for a, b in combinations(card, 2):
+    if a != b and (a + b)%10 < score:
+      win +=1
+answer = win / total
 print(f"{answer:.3f}")
