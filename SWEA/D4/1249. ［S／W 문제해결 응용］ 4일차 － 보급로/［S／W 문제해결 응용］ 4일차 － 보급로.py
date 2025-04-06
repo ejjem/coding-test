d5f14fe2b6b4b64f345df9437cbb52c0d2c76742
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-04-07 03:15:42 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 215 ms, Memory: 64,768 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.py
@@ -1,4 +1,4 @@
-from collections import deque
+import heapq
 
 T = int(input())
 answer = []
@@ -6,17 +6,20 @@ for ts in range(1, T+1):
   N = int(input())
   data = [list(map(int, str(input()))) for _ in range(N)]
   price = [[float('inf')] * N for _ in range(N)]
-  queue = deque()
-  queue.append((0, 0, 0))
-  while queue:
-    x, y, w = queue.popleft()
+  price[0][0] = 0
+  heap = []
+  heapq.heappush(heap, (0, 0, 0))
+  while heap:
+    w, x, y = heapq.heappop(heap)
+    if w > price[x][y]:
+      continue
     dists = [(1, 0), (0, 1), (-1, 0), (0, -1)]
     for dx, dy in dists:
       nx, ny = x + dx, y + dy
       if 0 <= nx < N and 0 <= ny < N:
         tmp = data[nx][ny] + w
         if tmp < price[nx][ny]:
-          price[nx][ny] = tmp
-          queue.append((nx, ny, tmp))
+          price[nx][ny] = tmp   
+          heapq.heappush(heap, (tmp, nx, ny))
   answer.append(f"#{ts} {price[N-1][N-1]}")
 print("\n".join(answer))
