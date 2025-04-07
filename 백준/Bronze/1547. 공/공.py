39b9df94318b3886914217b6a4f132006c4040c6
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-04-07 23:46:16 +0900

    [Bronze III] Title: 공, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1547. 공/공.py
+++ 백준/Bronze/1547. 공/공.py
@@ -1,13 +1,10 @@
 import sys
 input = sys.stdin.readline
+number = list(range(1, 4))
 
 M = int(input())
-cup = [1, 2, 3]
 for _ in range(M):
-  i, j = map(int, input().split())
-  if i != j:
-    a = cup.index(i)
-    b = cup.index(j)
-    cup[a], cup[b] = cup[b], cup[a]
-  
-print(cup[0])
+  X, Y = map(int, input().split())
+  index_X, index_Y = number.index(X), number.index(Y)
+  number[index_X], number[index_Y] = number[index_Y], number[index_X]
+print(number[0])
