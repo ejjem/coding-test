60240ff75e30ac6399de3775279a5a1176c189a1
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-04-13 02:49:55 +0900

    [Silver IV] Title: 빙고, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.py
+++ 백준/Silver/2578. 빙고/빙고.py
@@ -1,41 +1,29 @@
 import sys
 input = sys.stdin.readline
-board = [[x for x in map(int, input().split())] for _ in range(5)]
-anounce = [x for _ in range(5) for x in map(int, input().split())]
-def start(board, anounce):
+
+board = [list(map(int, input().split())) for _ in range(5)]
+anounce = []
+for _ in range(5):
+  arr = list(map(int, input().split()))
+  anounce.extend(arr)
+
+for i in range(len(anounce)):
+  target = anounce[i]
   bingo = 0
+  for row in board:
+    if target in row:
+      row[row.index(target)] = -1
+    if all(number == -1 for number in row):
+      bingo += 1
+  for col in range(5):
+    check = [board[i][col] for i in range(5)]
+    if all(number == -1 for number in check):
+      bingo += 1
+  if all(board[x][x] == -1 for x in range(5)):
+    bingo += 1
+  if all(board[x][4-x] == -1 for x in range(5)):
+    bingo += 1
   
-  for i in range(len(anounce)):
-    target = anounce[i]
-    for j in range(5):
-      if target in board[j]:
-        board[j][board[j].index(target)] = -1
-    
-    if i >= 4:
-      for row in board:
-        if all(x == -1 for x in row):
-          bingo += 1
-          
-      for col in range(5):
-        if all(board[row][col] == -1 for row in range(5)):
-          bingo+=1
-            
-      if all(board[k][k] == -1 for k in range(5)):
-        bingo += 1
-
-      if all(board[k][4-k] == -1 for k in range(5)):
-        bingo += 1
-        
-      if bingo >= 3:
-        print(i + 1)
-        return
-      else:
-          bingo = 0
-        
-
-start(board, anounce)
-    
-  
-
-
-
+  if bingo >= 3:
+    print(i+1)
+    break
