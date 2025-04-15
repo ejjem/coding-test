7fd4d1fb4309479b9dc9727be71a602b8eb18216
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-04-16 04:57:20 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 71 ms, Memory: 53,632 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -21,10 +21,7 @@ for ts in range(1, T + 1):
             else:
               break
           if len(tmp) == K:
-            left = col - 1
-            right = col + K
-            if (left < 0 or board[row][left] == 0) and (right >= N or board[row][right] == 0):
-              answer += 1
+            answer += 1
         if (row, col) not in visited_2:
           visited_2.add((row, col))
           tmp2 = [(row, col)]
@@ -38,8 +35,5 @@ for ts in range(1, T + 1):
             else:
               break
           if len(tmp2) == K:
-            top = row - 1
-            bottom = row + K
-            if (top < 0 or board[top][col] == 0) and (bottom >= N or board[bottom][col] == 0):
-              answer += 1
+            answer += 1
   print(f"#{ts} {answer}")
