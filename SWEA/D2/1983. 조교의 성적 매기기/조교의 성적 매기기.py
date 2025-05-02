1980bd75faa5fceb4d60af48fe39c6d57a212ea7
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-03 04:38:43 +0900

    [D2] Title: 조교의 성적 매기기, Time: 69 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,33 +1,17 @@
 T = int(input())
-for tc in range(1, T+1):
-  student = []
-  N, K = map(int, input().split())
-  students = []
-  for i in range(N):
-    a, b, c = map(int, input().split())
-    score = (a*35)/100 + (b*45)/100 + (c*20)/100
-    student.append((i+1, score))
-  student.sort(key=lambda x: x[1])
-  for i in range(N):
-    idx, score = student[i]
-    if idx == K:
-      if 0 <= i <= N//10-1:
-        print(f"#{tc} D0")
-      elif N//10 <= i <= 2*N//10-1:
-        print(f"#{tc} C-")
-      elif 2*N//10 <= i <= 3*N//10-1:
-        print(f"#{tc} C0")
-      elif 3*N//10 <= i <= 4*N//10-1:
-        print(f"#{tc} C+")
-      elif 4*N//10 <= i <= 5*N//10-1:
-        print(f"#{tc} B-")
-      elif 5*N//10 <= i <= 6*N//10-1:
-        print(f"#{tc} B0")
-      elif 6*N//10 <= i <= 7*N//10-1:
-        print(f"#{tc} B+")
-      elif 7*N//10 <= i <= 8*N//10-1:
-        print(f"#{tc} A-")
-      elif 8*N//10 <= i <= 9*N//10-1:
-        print(f"#{tc} A0")
-      elif 9*N//10 <= i <= 10*N//10-1:
-        print(f"#{tc} A+")
+grades = ["A+", "A0", "A-", "B+", "B0", "B-", "C+", "C0", "C-", "D0"]
+for tc in range(1, T + 1):
+    N, K = map(int, input().split())
+    student = []
+    for i in range(N):
+        a, b, c = map(int, input().split())
+        score = a * 0.35 + b * 0.45 + c * 0.2
+        student.append((i + 1, score))
+    student.sort(key=lambda x: x[1], reverse=True)
+    for i in range(N):
+        if student[i][0] == K:
+            rank = i
+            break
+    group_size = N // 10
+    grade_index = rank // group_size
+    print(f"#{tc} {grades[grade_index]}")
