d67d6a92c62397a9a758b44f82ae47dac3ce013a
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-03 01:05:21 +0900

    [D4] Title: 가능한 시험 점수, Time: 178 ms, Memory: 59,648 KB -BaekjoonHub

--- SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.py
+++ SWEA/D4/3752. 가능한 시험 점수/가능한 시험 점수.py
@@ -2,11 +2,12 @@ T = int(input())
 for tc in range(1, T+1):
   N = int(input())
   points = [x for x in map(int, input().split())]
-  possible = set()
-  possible.add(0)
+  max_score = sum(points)
+  dp = [0] * (max_score + 1)
+  dp[0] = 1
   for point in points:
-    tmp = set()
-    for p in possible:
-      tmp.add(p + point)
-    possible.update(tmp)
-  print(f"#{tc} {len(possible)}")
+    for i in range(max_score, -1, -1):
+      if dp[i] == 1:
+        dp[i + point] = 1 
+  count = sum(dp)
+  print(f"#{tc} {count}")
