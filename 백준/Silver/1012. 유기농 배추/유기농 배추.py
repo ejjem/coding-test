8e80855a26cb92c02e5a54f06373e0046172ee6f
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-07 02:23:58 +0900

    [Silver II] Title: 유기농 배추, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,41 +1,30 @@
 import sys
 input = sys.stdin.readline
-
 T = int(input())
-answer = []
-
-def search(bat, M, N):
-    checked = set()
-    count = 0
-    for X in range(M):
-        for Y in range(N):
-            if bat[Y][X] == 1 and (X, Y) not in checked:
-                DFS(X, Y, checked, bat, M, N)
-                count += 1
-    answer.append(count)      
-
-def DFS(X, Y, checked, bat, M, N):
-    stack = [(X, Y)]
-    checked.add((X, Y))
-
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-    while stack:
-        x, y = stack.pop()
-        for dx, dy in directions:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < M and 0 <= ny < N:
-                if bat[ny][nx] == 1 and (nx, ny) not in checked:
-                    checked.add((nx, ny))
-                    stack.append((nx, ny))
-
 for _ in range(T):
-    M, N, K = map(int, input().split())
-    bat = [[0] * M for _ in range(N)]  
-    for _ in range(K):
-        X, Y = map(int, input().split())
-        bat[Y][X] = 1
-    search(bat, M, N)
-
-for i in answer:
-    print(i)
+  M, N, K = map(int, input().split())
+  board = [ [0] * M for _ in range(N)]
+  for _ in range(K):
+    x, y = map(int, input().split())
+    board[y][x] = 1
+  visited = set()
+  answer = 0
+  def DFS(start_x, start_y):
+    global visited, answer, board
+    stack = [(start_x, start_y)]
+    direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    while stack:
+      x, y = stack.pop()
+      if board[y][x] == 1 and (x, y) not in visited:
+        visited.add((x, y))
+        for dx, dy in direction:
+          X, Y = dx + x, dy + y
+          if 0 <= X <= M-1 and 0<= Y <= N-1:
+            if board[Y][X] == 1 and (X, Y) not in visited:
+              stack.append((X, Y))
+  for y in range(N):
+    for x in range(M):
+      if board[y][x] == 1 and (x, y) not in visited:
+        DFS(x, y)
+        answer += 1
+  print(answer)
