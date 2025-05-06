26d543e2426064ff657ab43e22087cedd390ab33
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-07 03:25:20 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 162 ms, Memory: 66,304 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -3,13 +3,12 @@ for tc in range(1, T+1):
   board = [ list(map(int, input().split())) for _ in range(4)]
   possible = set()
   direction = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-  def DFS(start_x, start_y):
-    global board, possible, direction
+  def DFS(start_x, start_y, board, possible, direction):
     tmp = str(board[start_y][start_x])
     stack = [(start_x, start_y, tmp, 0)]
     while stack:
       x, y, string, count = stack.pop()
-      if count == 6 and string not in possible:
+      if count == 6:
         possible.add(string)
       if count < 6:
         for dx, dy in direction:
@@ -18,5 +17,5 @@ for tc in range(1, T+1):
             stack.append((X, Y, string + str(board[Y][X]), count + 1))
   for x in range(4):
     for y in range(4):
-      DFS(x, y) 
+      DFS(x, y, board, possible, direction) 
   print(f"#{tc} {len(possible)}")
