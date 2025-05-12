5bd177f868800c90bb7683b47a64cd4ccb3abc4d
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-12 16:53:18 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,20 +1,10 @@
 import sys
 input = sys.stdin.readline
-
-n = int(input())
-num1 = 0
-num2 = 1
-# 0, 1, 1, 2, 3, 5, ...
-for i in range(2, n + 1):
-  if i % 2 == 0:
-    num1 = num1 + num2
-  else:
-    num2 = num1 + num2
-
-if n % 2 == 0:
-  print(num1)
-else:
-  print(num2)
-  
-  
-  
+N = int(input())
+dp = [0] * (N+1)
+dp[0] = 0
+dp[1] = 1
+if N >= 2:
+    for i in range(2, len(dp)):
+        dp[i] = dp[i-1] + dp[i-2]
+print(dp[N])
