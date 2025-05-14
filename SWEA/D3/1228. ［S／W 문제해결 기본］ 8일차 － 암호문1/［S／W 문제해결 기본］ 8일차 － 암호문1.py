1b8542d438ddc6ae1d2b24602b3d1fbb1f4a02da
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-15 01:34:45 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 56 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -1,29 +1,16 @@
 for tc in range(1, 11):
     N = int(input())
-    origin = list(map(int, input().split()))
-    num_of_inst = int(input())
-    origin_inst = list(input().split())
-    index = []
-    insts = []
-    tmp = []
-    answer = []
-    for i in range(len(origin)):
-        answer.append(origin[i])
-    for i in range(len(origin_inst)):
-        if origin_inst[i] == 'I':
-            index.append(i)
-    index.append(len(origin_inst))
-    for i in range(1, len(index)):
-        insts.append(origin_inst[index[i-1]+1:index[i]])
-    for inst in insts:
-        tmp = []
-        x = int(inst[0])
-        y = int(inst[1])
-        s = list(map(int, inst[2:]))
-        tmp.extend(answer[:x])
-        tmp.extend(s)
-        tmp.extend(answer[x:])
-        answer = []
-        for i in range(len(tmp)):
-            answer.append(tmp[i])
-    print(f"#{tc} {' '.join(map(str, answer[:10]))}")
+    origin = list(input().split())
+    num_inst = int(input())
+    inst = list(input().split())
+    i = 0
+    while i < len(inst):
+        if inst[i] == 'I':
+            x = int(inst[i+1])
+            y = int(inst[i+2])
+            s = inst[i+3:i+3+y]
+            origin[x:x] = s
+            i += 3+y
+        else:
+            i += 1
+    print(f"#{tc} {' '.join(origin[:10])}")
