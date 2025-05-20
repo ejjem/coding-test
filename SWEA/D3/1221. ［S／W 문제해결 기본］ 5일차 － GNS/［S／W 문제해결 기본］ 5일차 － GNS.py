b9ebf3aabe8058b93e2c9a70162ada85c9d19dab
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-21 02:30:10 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 82 ms, Memory: 61,568 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.py
@@ -3,12 +3,12 @@ num = ["ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"]
 for tc in range(1, T+1):
     testcase, N = input().split()
     number = list(input().split())
-    real_number = []
+    count = [0] * 10
     answer = []
     for string in number:
-        real_number.append(num.index(string))
-    real_number.sort()
-    for a in real_number:
-        answer.append(num[a])
+        count[num.index(string)] += 1
+    for i in range(10):
+        tmp = [num[i]] * count[i]
+        answer.extend(tmp)
     print(f"#{tc}")
     print(' '.join(answer))
