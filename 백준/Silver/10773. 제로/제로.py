a24a0ea19b0be8f463bf87c6dc2fa8a87f2aefda
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-05-22 13:44:55 +0900

    [Silver IV] Title: 제로, Time: 64 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+K = int(input())
+stack = []
+for _ in range(K):
+    tmp = int(input())
+    if tmp == 0:
+        stack.pop()
+    else:
+        stack.append(tmp)
+print(sum(stack))
