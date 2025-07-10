3a4e348ad94218078fc9e5b9e22770a91f909c21
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-07-10 20:33:15 +0900

    [D1] Title: 자릿수 더하기, Time: 60 ms, Memory: 51,968 KB -BaekjoonHub

--- SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
+++ SWEA/D1/2058. 자릿수 더하기/자릿수 더하기.py
@@ -1,2 +1,15 @@
-number = list(input())
-print(sum(map(int, number)))
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String S = sc.nextLine();
+		int answer = 0;
+		for(int i=0; i<S.length(); i++) {
+			answer += S.charAt(i) - '0';
+		}
+		System.out.println(answer);
+		sc.close();
+	}
+}
