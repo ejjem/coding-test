fe9c746c8ad3a51b413743a7c7c70b545e004037
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-07-10 20:26:32 +0900

    [D1] Title: 중간값 찾기, Time: 60 ms, Memory: 53,760 KB -BaekjoonHub

--- SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
@@ -1,4 +1,21 @@
-N = int(input())
-number = list(map(int, input().split()))
-number.sort()
-print(number[N//2])
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] scores = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			scores[i] = sc.nextInt();
+		}
+		Arrays.sort(scores);
+		int median = scores[N/2];
+		System.out.println(median);
+		
+		sc.close();
+	}
+
+}
