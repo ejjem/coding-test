a1024f3816ffea43d40f2e3450246160936f28f2
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-07-15 21:13:19 +0900

    [Bronze V] Title: ??!, Time: 104 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main{
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    String id = br.readLine();
+
+    System.out.println( id + "??!");
+
+  }
+}
