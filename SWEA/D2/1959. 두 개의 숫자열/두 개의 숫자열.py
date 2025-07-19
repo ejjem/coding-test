3aa62de8a523ce072fb2858e498fa970037a6a03
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-07-19 17:33:50 +0900

    [D2] Title: 두 개의 숫자열, Time: 59 ms, Memory: 52,480 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,22 +1,18 @@
 T = int(input())
-for tc in range(1, T+1):
+for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    Ai = list(map(int, input().split()))
-    Bj = list(map(int, input().split()))
-    maximum = -float('inf')
-    if N <= M:
-        for i in range(M - N + 1):
-            arr = Bj[i:i+N]
-            tmp = 0
-            for j in range(N):
-                tmp += arr[j] * Ai[j]
-            maximum = max(maximum, tmp)
-        print(f"#{tc} {maximum}")
-    else:
-        for i in range(N - M + 1):
-            arr = Ai[i:i+N]
-            tmp = 0
-            for j in range(M):
-                tmp += arr[j] * Bj[j]
-            maximum = max(maximum, tmp)
-        print(f"#{tc} {maximum}")
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    if N > M:
+        A, B = B, A
+        N, M = M, N
+
+    max_sum = -float('inf')
+    for i in range(M - N + 1):
+        temp_sum = 0
+        for j in range(N):
+            temp_sum += A[j] * B[i + j]
+        max_sum = max(max_sum, temp_sum)
+
+    print(f"#{tc} {max_sum}")
