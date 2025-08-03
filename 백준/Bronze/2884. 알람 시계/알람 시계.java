74f854b74e9033fe28ab0a6d652c9cec11f05c04
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-03 15:19:02 +0900

    [Bronze III] Title: 알람 시계, Time: 112 ms, Memory: 14440 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int H = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		if (M >= 45) {
+			M -= 45;
+		}
+		else {
+			if (H == 0) {
+				H = 23;
+				M = 60 - (45 - M);
+			}
+			else {
+				H -= 1;
+				M = 60 - (45 - M);
+			}
+		}
+		System.out.printf("%d %d", H, M);
+	}
+}
