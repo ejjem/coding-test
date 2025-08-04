782ebafe37e199642b840582c7db461efb009684
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-04 20:37:37 +0900

    [Bronze V] Title: A+B - 5, Time: 108 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        ArrayList<Integer> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = 0;
+        int b = 0;
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            if (a == 0 && b == 0) return;
+            System.out.println(a+b);
+        }
+    }
+}
