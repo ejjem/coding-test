dfece01fbc1ccab68e55e9be1e7f1fd44d502c31
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-04 20:45:48 +0900

    [Bronze V] Title: 아스키 코드, Time: 108 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception{
+        ArrayList<Integer> list = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char tmp = br.readLine().charAt(0);
+        System.out.println((int)tmp);
+    }
+}
