9908398358321910004e9ebd54d7170b65c9346a
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-06 15:35:21 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1132 ms, Memory: 59484 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] sum_array = new int[N];
+        sum_array[0] = Integer.parseInt(st.nextToken());
+        for(int idx=1; idx<N; idx++){
+            sum_array[idx] = sum_array[idx-1] + Integer.parseInt(st.nextToken());
+        }
+        for(int idx=0; idx<M; idx++){
+            st = new  StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            if (i == 1){
+                System.out.println(sum_array[j-1]);
+            }
+            else System.out.println(sum_array[j-1] - sum_array[i-2]);
+        }
+        
+    }
+}
