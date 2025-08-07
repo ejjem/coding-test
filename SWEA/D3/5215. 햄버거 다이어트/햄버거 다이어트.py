0c6762117147968c9232625c9f4b7ca8ee82436c
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-07 16:55:41 +0900

    [D3] Title: 햄버거 다이어트, Time: 85 ms, Memory: 59,264 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,12 +1,37 @@
-T = int(input())
-total_answer = []
-for ts in range(1, T+1):
-  N, L = map(int, input().split())
-  ingredients = [tuple(map(int, input().split())) for _ in range(N)]
-  dp = [0] * (L+1)
-  for point, cal in ingredients:
-    for c in range(L, cal-1, -1):
-      dp[c] = max(dp[c], dp[c-cal] + point)  
-  total_answer.append(f"#{ts} {max(dp)}")
+import java.util.*;
+import java.io.*;
 
-print("\n".join(total_answer))
+class Solution {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+        for(int tc=1;tc<T+1;tc++){
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int L = Integer.parseInt(st.nextToken());
+            int[] score = new int[N];
+            int[] kcal = new int[N];
+            int[] dp =  new int[L+1];
+            for(int i=0;i<N;i++){
+                st = new StringTokenizer(br.readLine());
+                score[i] = Integer.parseInt(st.nextToken());
+                kcal[i] = Integer.parseInt(st.nextToken());
+            }
+            for(int idx_arr=0; idx_arr<N; idx_arr++){
+                for(int idx_dp=L; idx_dp>kcal[idx_arr] - 1; idx_dp--){
+                    dp[idx_dp] = Math.max(dp[idx_dp], dp[idx_dp - kcal[idx_arr]] + score[idx_arr] );
+                }
+            }
+            int maximum = Integer.MIN_VALUE;
+            for(int i=0; i<L+1; i++){
+                maximum = Math.max(maximum, dp[i]);
+            }
+            sb.append("#").append(tc).append(" ").append(maximum).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
