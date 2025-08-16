5128dd75aeb15ff0de69492a09fead39f17abe80
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-16 11:19:00 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 75 ms, Memory: 59,520 KB -BaekjoonHub

--- SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -1,13 +1,33 @@
-T = int(input())
-for tc in range(1, T+1):
-    N = int(input())
-    count = 1
-    number = set()
-    while True:
-        if len(number) == 10:
-            print(f"#{tc} { (count-1) * N}")
-            break
-        arr = list(str(N*count))
-        for i in range(len(arr)):
-            number.add(arr[i])
-        count += 1
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Solution {
+    static BufferedReader br;
+    static StringBuilder sb;
+    static int end;
+    
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        end = (1 << 10) - 1;
+        for(int tc=1; tc<T+1; tc++){
+            int N = Integer.parseInt(br.readLine());
+            int tmp = 0;
+            int cnt = 1;
+            while(true){
+                char[] ch = String.valueOf(N * cnt).toCharArray();
+                for(int i=0; i<ch.length; i++) {
+                	int num = ch[i] - '0';
+                	tmp = tmp | (1 << num);
+                }
+                if(tmp == end) break;
+                cnt ++;
+            }
+            sb.append("#").append(tc).append(" ").append(cnt * N).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
