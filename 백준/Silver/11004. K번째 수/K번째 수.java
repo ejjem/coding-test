b5d4cb6efc0ebad0be4fc0b6eeacc1905447b454
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-23 20:52:05 +0900

    [Silver V] Title: K번째 수, Time: 1052 ms, Memory: 368420 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,22 +1,51 @@
 import java.util.*;
+import java.lang.*;
 import java.io.*;
 
-public class Main {
-    static BufferedReader br;
-    static BufferedWriter bw;
-    static StringTokenizer st;
-    static StringBuilder sb;
+class Main {
+    static int[] nums;
+    static void QuickSort(int start, int end, int K) {
+    	if(start < end) {
+    		int pivot = partition(start, end);
+    		if(pivot == K) return;
+    		else if(K < pivot) QuickSort(start, pivot - 1, K);
+    		else QuickSort(pivot+1, end, K);
+    		}
+    }
+    
+    static int partition(int start, int end) {
+    	if(start + 1 == end) {
+    		if(nums[start] > nums[end]) Swap(start, end);
+    		return end;
+    	}
+    	int middle = (start + end) / 2;
+    	Swap(start, middle);
+    	int pivot = nums[start];
+    	int i = start + 1, j = end;
+    	while(i<=j) {
+    		while(j >= start + 1 && pivot < nums[j]) j--;
+    		while(i <= end && pivot > nums[i]) i++;
+    		if(i<=j) Swap(i++, j--);
+    	}
+    	nums[start] = nums[j];
+    	nums[j] = pivot;
+    	return j;
+    }
+    
+    static void Swap(int a, int b) {
+    	int tmp = nums[a]; nums[a] = nums[b]; nums[b] = tmp;
+    }
     public static void main(String[] args) throws Exception {
-        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        int[] nums = new int[N];
+        int N = Integer.parseInt(st.nextToken()); int K = Integer.parseInt(st.nextToken());
+        nums = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-        	nums[i] = Integer.parseInt(st.nextToken());
-        }
-        Arrays.sort(nums);
+        for(int i=0;i<N;i++) nums[i] = Integer.parseInt(st.nextToken());
+        
+        QuickSort(0, N-1, K-1);
         System.out.println(nums[K-1]);
+       
     }
 }
