e1f256b361b39aa5b40c36ee154f790b7b382230
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-26 16:38:12 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 464 ms, Memory: 112900 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -5,44 +5,57 @@ public class Main {
 	static int N, M;
 	static HashMap<Integer, ArrayList<Integer>> graph;
 	static boolean[] visited;
-	static ArrayDeque<Integer> stack;
-	static void DFS(int start) {
-		visited[start] = true;
-		stack.offerLast(start);
-		while(!stack.isEmpty()) {
-			int tmp = stack.pollLast();
-			if(graph.containsKey(tmp)) {
-				for(int node : graph.get(tmp)) {
-					if(!visited[node]) {
-						visited[node] = true;
-						stack.offerLast(node);
-					}
+	
+	static class UnionFind {
+		private int[] parents;
+		private int[] rank;
+		
+		UnionFind(int N) {
+			this.parents = new int[N];
+			this.rank = new int[N];
+			makeSet(N);
+		}
+		private void makeSet(int N) {
+			for(int i=0; i<N; i++) {
+				parents[i] = i;
+			}
+		}
+		
+		public int findSet(int x) {
+			if(parents[x] != x) parents[x] = findSet(parents[x]);
+			return parents[x];
+		}
+		
+		public void union(int a, int b) {
+			int rootA = findSet(a);
+			int rootB = findSet(b);
+			if(rootA == rootB) return;
+			else {
+				if(rank[rootA] > rank[rootB]) parents[rootB] = rootA;
+				else if(rank[rootA] < rank[rootB]) parents[rootA] = rootB;
+				else {
+					parents[rootB] = rootA;
+					rank[rootA] ++;
 				}
 			}
 		}
 	}
 	
-	
 	public static void main(String[] args) throws Exception {
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	StringTokenizer st = new StringTokenizer(br.readLine());
-	N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
-	graph = new HashMap<>();
-	for(int i=0; i<M; i++) {
-		st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken()) - 1; int b= Integer.parseInt(st.nextToken()) - 1;
-		graph.computeIfAbsent(a, x -> new ArrayList<Integer>()).add(b);
-		graph.computeIfAbsent(b, x -> new ArrayList<Integer>()).add(a);
-	}
-	stack = new ArrayDeque<>();
-	visited = new boolean[N];
-	int cnt = 0;
-	for(int i=0; i<N; i++) {
-		if(!visited[i]) {
-			DFS(i);
-			cnt++;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
+		graph = new HashMap<>();
+		UnionFind uf = new UnionFind(N);
+		for(int i=0; i<M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken()) - 1; int b= Integer.parseInt(st.nextToken()) - 1;
+			uf.union(a, b);
 		}
-	}
-	System.out.println(cnt);
+		HashSet<Integer> root = new HashSet<>();
+		for(int i=0; i<N; i++) {
+			root.add(uf.findSet(i));
+		}
+		System.out.println(root.size());
 	}
 }
