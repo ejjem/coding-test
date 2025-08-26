7ea1417400e62b065d8cd6a141708af04728e02c
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-08-26 16:29:45 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 604 ms, Memory: 155976 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,64 +1,48 @@
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.*;
 import java.io.*;
 
 public class Main {
-	static BufferedReader br;
-	static StringTokenizer st;
+	static int N, M;
+	static HashMap<Integer, ArrayList<Integer>> graph;
 	static boolean[] visited;
-	static boolean[][] graph;
-	static int cnt;
 	static ArrayDeque<Integer> stack;
-	static int N;
-	static int M;
-	
-	static void dfs(int start) {
-		if(visited[start] == true) return;
-		else {
-			visited[start] = true;
-			for(int i=0; i< N; i++) {
-				if(start != i && graph[start][i] == true) {
-					if(!visited[i]) stack.offerLast(i);
-				}
-			}
-			while(true) {
-				if(stack.isEmpty()) return;
-				else {
-				int target = stack.pollLast();
-				dfs(target);
+	static void DFS(int start) {
+		visited[start] = true;
+		stack.offerLast(start);
+		while(!stack.isEmpty()) {
+			int tmp = stack.pollLast();
+			if(graph.containsKey(tmp)) {
+				for(int node : graph.get(tmp)) {
+					if(!visited[node]) {
+						visited[node] = true;
+						stack.offerLast(node);
+					}
 				}
 			}
 		}
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException, Exception {
-		br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void main(String[] args) throws Exception {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
+	N = Integer.parseInt(st.nextToken()); M = Integer.parseInt(st.nextToken());
+	graph = new HashMap<>();
+	for(int i=0; i<M; i++) {
 		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		graph = new boolean[N][N];
-		visited = new boolean[N];
-		stack = new ArrayDeque<>();
-		cnt = 0;
-		for(int i=0; i<N; i++) {
-			graph[i][i] = true;
-		}
-		for(int i=0; i<M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken()) - 1;
-			int v = Integer.parseInt(st.nextToken()) - 1;
-			graph[u][v] = true;
-			graph[v][u] = true;
-		}
-		for(int i=0; i<N; i++) {
-			if(visited[i] == true) continue;
-			else {
-				cnt ++;
-				dfs(i);
-			}
-		}
-		System.out.println(cnt);
+		int a = Integer.parseInt(st.nextToken()) - 1; int b= Integer.parseInt(st.nextToken()) - 1;
+		graph.computeIfAbsent(a, x -> new ArrayList<Integer>()).add(b);
+		graph.computeIfAbsent(b, x -> new ArrayList<Integer>()).add(a);
 	}
-
+	stack = new ArrayDeque<>();
+	visited = new boolean[N];
+	int cnt = 0;
+	for(int i=0; i<N; i++) {
+		if(!visited[i]) {
+			DFS(i);
+			cnt++;
+		}
+	}
+	System.out.println(cnt);
+	}
 }
