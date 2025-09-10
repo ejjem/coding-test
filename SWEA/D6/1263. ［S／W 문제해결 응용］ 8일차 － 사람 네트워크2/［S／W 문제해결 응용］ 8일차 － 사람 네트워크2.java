d9eb28aea9a3e58e36c4a5e03402062175952b5b
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-09-10 15:18:17 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,351 ms, Memory: 102,836 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-
+ 
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -8,35 +8,31 @@ public class Solution {
         StringTokenizer st;
         int T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
-        	int answer = Integer.MAX_VALUE;
-        	st = new StringTokenizer(br.readLine());
-        	int N = Integer.parseInt(st.nextToken());
-        	int[][] graph = new int[N][N];
-        	
-        	for(int i=0; i<N; i++) {
-        		for(int j=0; j<N; j++) {
-        			if(Integer.parseInt(st.nextToken()) == 1) graph[i][j] = 1;
-        			else if(i != j) graph[i][j] = 10000;
-        		}
-        	}
-        	
-        	for(int k=0; k<N; k++) {
-        		for(int i=0; i<N; i++) {
-        			for(int j=0; j<N; j++) {
-        				if(i == j) continue;
-        				if(graph[i][k] != Integer.MAX_VALUE && graph[k][j] != Integer.MAX_VALUE) {
-        					graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
-        				}
-        			}
-        		}
-        	}
-        	for(int i=0; i<N; i++) {
-        		int tmp = 0;
-        		for(int j=0; j<N; j++) {
-        			if(graph[i][j] != 10000) tmp += graph[i][j];
-        		}
-        		answer = Math.min(answer, tmp);
-        	}
+            int answer = Integer.MAX_VALUE;
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int[][] graph = new int[N][N];
+            for(int i=0; i<N; i++) {
+                for(int j=0; j<N; j++) {
+                    if(Integer.parseInt(st.nextToken()) == 1) graph[i][j] = 1;
+                    else if(i != j) graph[i][j] = 10000;
+                }
+            }
+             
+            for(int k=0; k<N; k++) {
+                for(int i=0; i<N; i++) {
+                    for(int j=0; j<N; j++) {
+                    	graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+                    }
+                }
+            }
+            for(int i=0; i<N; i++) {
+                int tmp = 0;
+                for(int j=0; j<N; j++) {
+                    tmp += graph[i][j];
+                }
+                answer = Math.min(answer, tmp);
+            }
             sb.append('#').append(tc).append(' ').append(answer).append('\n');
         }
         System.out.print(sb.toString());
