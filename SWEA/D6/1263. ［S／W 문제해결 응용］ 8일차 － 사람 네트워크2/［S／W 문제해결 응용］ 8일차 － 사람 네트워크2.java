fc4db29bdb072cf5473d856a2e314d750c7820ab
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-09-10 14:50:09 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 3,679 ms, Memory: 102,512 KB -BaekjoonHub

---
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+        	int answer = Integer.MAX_VALUE;
+        	st = new StringTokenizer(br.readLine());
+        	int N = Integer.parseInt(st.nextToken());
+        	int[][] graph = new int[N][N];
+        	for(int i=0; i<N; i++) {
+        		Arrays.fill(graph[i], Integer.MAX_VALUE);
+        		graph[i][i] = 0;
+        	}
+        	for(int i=0; i<N; i++) {
+        		for(int j=0; j<N; j++) {
+        			if(Integer.parseInt(st.nextToken()) == 1) graph[i][j] = 1;
+        		}
+        	}
+        	
+        	for(int k=0; k<N; k++) {
+        		for(int i=0; i<N; i++) {
+        			for(int j=0; j<N; j++) {
+        				if(i == j) continue;
+        				if(graph[i][k] != Integer.MAX_VALUE && graph[k][j] != Integer.MAX_VALUE) {
+        					graph[i][j] = Math.min(graph[i][j], graph[i][k] + graph[k][j]);
+        				}
+        			}
+        		}
+        	}
+        	for(int i=0; i<N; i++) {
+        		int tmp = 0;
+        		for(int j=0; j<N; j++) {
+        			if(graph[i][j] != Integer.MAX_VALUE) tmp += graph[i][j];
+        		}
+        		answer = Math.min(answer, tmp);
+        	}
+            sb.append('#').append(tc).append(' ').append(answer).append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+}
