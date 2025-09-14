ebbe482bf13cecc1df665dc8e7557326add18bc6
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-09-15 08:44:45 +0900

    [Bronze V] Title: 이상한 기호, Time: 104 ms, Memory: 14200 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.java
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -2,12 +2,12 @@ import java.util.*;
 import java.io.*;
 
 class Main {
+	
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        long answer = (a + b) * (a - b);
-        System.out.println(answer);
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        System.out.println((a + b) * (a - b));
     }
 }
