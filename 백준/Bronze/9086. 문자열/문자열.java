8acc4576c6b56503765c9717867ef3367be910d8
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-09-14 22:24:32 +0900

    [Bronze V] Title: 문자열, Time: 96 ms, Memory: 14068 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+            sb.append(str.charAt(0)).append(str.charAt(str.length()-1)).append("\n");
+        }
+        bw.write(sb.toString()); bw.flush(); bw.close();
+    }
+}
