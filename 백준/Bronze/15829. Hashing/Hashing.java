662c1160e9cbbe23b426ae875b349963e382ac74
Jemin Lee <147822576+ejjem@users.noreply.github.com>
2025-11-22 02:49:01 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14200 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,15 +1,18 @@
 import java.io.*;
 class Main {
+    static long MOD = 1234567891;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         String str = br.readLine();
-        int hash = 0;
+        long hash = 0;
+        long pow = 1;
         for(int i=0; i<n; i++){
             char c = str.charAt(i);
-            int tmp = c - 'a' + 1;
-            hash += tmp * (int)Math.pow(31, i) % 1234567891;
+            long tmp = c - 'a' + 1;
+            hash += tmp*pow % MOD;
+            pow = pow * 31 % MOD; 
         }
-        System.out.println(hash%1234567891);
+        System.out.println(hash%MOD);
     }
 }
